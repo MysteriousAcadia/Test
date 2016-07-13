54de16a45a17a59c2d65ca3a10fd9a96d060107c
prateek <emailprateeksharma@gmail.com>
2016-07-13 19:58:51 +0530

    basic auth and a few migrations

--- database/migrations/2016_07_13_141444_create_eggs_table.php
+++ database/migrations/2016_07_13_141444_create_eggs_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateEggsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('eggs', function(Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->boolean('is_admin')->default(0);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('eggs');
     }
 }
